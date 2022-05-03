8ebaff31c63f54c3523dd18206eb7d3dbd04f1de
Miguel333YT <97993047+Miguel333YT@users.noreply.github.com>
2022-05-02 21:24:39 -0500

    Add files via upload

---
+++ Recolectar-tesoros-2-plantilla-main/sketch.js
@@ -0,0 +1,157 @@
+//aqui se crean las variables
+var path,boy,cash,diamonds,jwellery,sword;
+var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
+var treasureCollection = 0;
+var cashG,diamondsG,jwelleryG,swordGroup;
+
+//Game States (estados del juego)
+var PLAY=1;
+var END=0;
+var gameState=1;
+
+//aqui se añaden las imagenes al codigo
+function preload(){
+  pathImg = loadImage("Road.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jwelleryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  gover = loadImage("gameOver.png");
+  
+  //gover = loadImage("gameOver.png");
+}
+
+function setup(){
+  
+  createCanvas(400,600);
+// Mover el fondo
+  
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+
+
+//crear sprite de niño corriendo
+boy = createSprite(70,580,20,20);
+boy.addAnimation("SahilRunning",boyImg);
+boy.scale=0.08;
+  
+
+//aqui se crean los grupos de objetos
+cashG=new Group();
+diamondsG=new Group();
+jwelleryG=new Group();
+swordGroup=new Group();
+
+}
+
+function draw() {
+
+  if(gameState===PLAY){
+  background(0);
+  boy.x = World.mouseX;
+  
+  //aqui se crean los bordes y la configuracion para que boy colisione con ellos
+  edges= createEdgeSprites();
+  boy.collide(edges);
+  
+  //código para reiniciar el fondo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+    createCash();
+    createDiamonds();
+    createJwellery();
+    createSword();
+
+    //aqui se añade el puntaje que obtiene boy al recolectar los tesoros
+    if (cashG.isTouching(boy)) {
+      cashG.destroyEach();
+      treasureCollection=treasureCollection+50;
+    }
+    else if (diamondsG.isTouching(boy)) {
+      diamondsG.destroyEach();
+      treasureCollection=treasureCollection+100;
+      
+    }else if(jwelleryG.isTouching(boy)) {
+      jwelleryG.destroyEach();
+      treasureCollection= treasureCollection + 150;
+      
+    }else{
+      if(swordGroup.isTouching(boy)) {
+        gameState=END;
+       
+        
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jwelleryG.destroyEach();
+        swordGroup.destroyEach();
+        
+        cashG.setVelocityYEach(0);
+        diamondsG.setVelocityYEach(0);
+        jwelleryG.setVelocityYEach(0);
+        swordGroup.setVelocityYEach(0);
+        
+        //aqui esta la imagen del game over
+          over = createSprite(180,280,20,20);
+          over.addAnimation("gameover",gover);
+          over.scale=0.7
+
+     
+    }
+  }
+  
+  drawSprites();
+  textSize(20);
+  fill(255);
+  text("Tesoro: "+ treasureCollection,10,30);
+  }
+
+}
+
+//aqui se crean los prites de los tesoros, su imagen, tamaño, velocidad, lifetime, grupo, y el codigo que los hace aparecer aleatoriamente
+function createCash() {
+  if (World.frameCount % 200 == 0) {
+  var cash = createSprite(Math.round(random(50, 350),40, 10, 10));
+  cash.addImage(cashImg);
+  cash.scale=0.12;
+  cash.velocityY = 3;
+  cash.lifetime = 150;
+  cashG.add(cash);
+  }
+}
+
+function createDiamonds() {
+  if (World.frameCount % 320 == 0) {
+  var diamonds = createSprite(Math.round(random(50, 350),40, 10, 10));
+  diamonds.addImage(diamondsImg);
+  diamonds.scale=0.03;
+  diamonds.velocityY = 3;
+  diamonds.lifetime = 150;
+  diamondsG.add(diamonds);
+}
+}
+
+function createJwellery() {
+  if (World.frameCount % 410 == 0) {
+  var jwellery = createSprite(Math.round(random(50, 350),40, 10, 10));
+  jwellery.addImage(jwelleryImg);
+  jwellery.scale=0.13;
+  jwellery.velocityY = 3;
+  jwellery.lifetime = 150;
+  jwelleryG.add(jwellery);
+  }
+}
+
+function createSword(){
+  if (World.frameCount % 530 == 0) {
+  var sword = createSprite(Math.round(random(50, 350),40, 10, 10));
+  sword.addImage(swordImg);
+  sword.scale=0.1;
+  sword.velocityY = 3;
+  sword.lifetime = 150;
+  swordGroup.add(sword);
+  }
+}
